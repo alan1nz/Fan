62e9cc67fc0b479b8729c5b48b5949db83a685dc
alan1nz <pencao111@gmail.com>
2017-09-30 21:41:17 +1300

    Add files via upload

---
+++ 29-9/311_Sample/311_Sample/PID.h
@@ -0,0 +1,25 @@
+/*
+ * PID.h
+ *
+ * Created: 9/26/2017 12:37:41 PM
+ *  Author: peng
+ */ 
+
+
+#ifndef PID_H_
+#define PID_H_
+
+#define p_coefficient 0.045
+#define i_coefficient 0.00015
+
+#define d_coefficient 1
+
+#define i_max 1000
+#define i_min 0
+
+
+#include <avr/io.h>
+
+void pid(int error,int sum_error, int pre_error);
+
+#endif /* PID_H_ */
